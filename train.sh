38c7204a8bb5d6668cfad8e8b6a5d696b354b94e
moser12138 <144081145+moser12138@users.noreply.github.com>
2024-11-07 10:15:14 +0800

    Update and rename dist_train.sh to train.sh

---
+++ train.sh
@@ -0,0 +1,9 @@
+## cityscapes
+export TORCH_DISTRIBUTED_DEBUG=DETAIL
+export CUDA_VISIBLE_DEVICES=0
+cfg_file=configs/cityscapes.py
+NGPUS=1
+torchrun --nproc_per_node=$NGPUS tools/train_city.py --config $cfg_file
+
+## camvid
+python tools/train_camvid.py --cfg configs/camvid.yaml
