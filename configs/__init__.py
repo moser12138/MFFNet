b944014a70325203b4ecfc0d0fba0fdffd9d6003
moser12138 <144081145+moser12138@users.noreply.github.com>
2024-12-27 09:34:41 +0800

    Update __init__.py

--- configs/__init__.py
+++ configs/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from default import _C as config
+from .default import _C as config
 
 import importlib
 
